66d8a85f4fab488f736ee27cb61bcc9430988743
Tizedes Zoltán <tizedeszoltan97@gmail.com>
2017-02-28 14:09:10 +0100

    Read logo from file

---
+++ 01-BaratokKozt/src/GnuTeam/BaratokKozt/TestReadFile.java
@@ -0,0 +1,41 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestReadFile {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException {
+
+		File file = new File("/home/tizedes/Desktop/JAVA/Test/src/baratok-kozt-logo.txt");
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		DataInputStream dis = null;
+
+		try {
+			fis = new FileInputStream(file);
+
+			bis = new BufferedInputStream(fis);
+			dis = new DataInputStream(bis);
+
+			while (dis.available() != 0) {
+
+				System.out.println(dis.readLine());
+			}
+
+			fis.close();
+			bis.close();
+			dis.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
